0ff7c528f368d970de94703d31861507d365db42
camilomelani <camilomelani@gmail.com>
2016-02-21 17:50:57 +0000

    Buen resultado: Atras, Frente, Frontal, Costado, Exterior, Interior, app webcam

--- app/detect_Auto.py
+++ app/detect_Auto.py
@@ -1,34 +1,41 @@
 #!/usr/bin/python
 
-#https://pythonprogramming.net/haar-cascade-object-detection-python-opencv-tutorial/
-#http://mark-kay.net/2014/06/24/detecting-vehicles-cctv-image/
 import numpy as np
 import cv2
 import sys
 from datetime import datetime
 import datetime
+import os
 
-inputfile = sys.argv[1]
+inputClasificator = sys.argv[1]
+inputdir = sys.argv[2]
 
 #this is the cascade we just made. Call what you want
-vehicle_classifier = cv2.CascadeClassifier('data/cascade.xml')
-
-gray = cv2.imread(inputfile,0)
+vehicle_classifier = cv2.CascadeClassifier(inputClasificator)
+detect = 0
+nodetect = 0
+outputDir = "imagenes_prueba_resultado/"
+for inputfile in os.listdir(inputdir):
+	gray = cv2.imread(inputdir + inputfile,0)
     
-a = datetime.datetime.now()
-autos = vehicle_classifier.detectMultiScale(gray, 3, 5, maxSize=(600,400),minSize=(300,200))
-b = datetime.datetime.now()
-print(b-a)
+#	a = datetime.datetime.now()
+	autos = vehicle_classifier.detectMultiScale(gray, 3, 5, maxSize=(600,400),minSize=(300,200))
+	if len(autos) > 0:
+		detect=detect+1
+	else:
+		nodetect=nodetect+1
+        
+	imagen = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+        for (x,y,w,h) in autos:
+            cv2.rectangle(imagen,(x,y),(x+w,y+h),(255,255,0),14)
+	
+	print outputDir + inputfile
+	cv2.imwrite(outputDir + inputfile,imagen)
+#	b = datetime.datetime.now()
 
-print 'Vehicles detected: %d' % (len(autos))
+print('Objetos detectados: %d Objetos No detectados: %d rate: %.2f' % (detect, nodetect,float(detect)/(detect+nodetect)))
     
-# add this
-for (x,y,w,h) in autos:
-  cv2.rectangle(gray,(x,y),(x+w,y+h),(255,255,0),2)
 
 
-cv2.imshow('img',gray)
 
-cv2.waitKey(3000)
 
-cv2.destroyAllWindows()
